11b53d38c0fa0ce820cf717458baf50e13915878
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-10-31 13:55:20 -0400

    style index.ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./views/*.ejs", "./views/partials/*.ejs"],
   theme: {
-    extend: {},
+    maxWidth: {
+      eighty: "80%",
+    },
+    extend: {
+      colors: {
+        "key-lime": "#EDEF8A",
+        "eaton-blue": "#99D19F",
+        "pale-spring-bud": "#E6E7A7",
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
+};
