5bcc62691805fd829a385fc0d9c869ee9210ac85
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-02-23 11:58:11 -0500

    add animated placeholder text

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,26 @@
-const searchBar = document.getElementById("searchBar");
+function animatePlaceholder() {
+  const searchBar = document.getElementById("searchBar");
+  const placeholder = searchBar.placeholder;
+  let index = 0;
 
-let index = 0;
+  const sentence = "What are you looking for?";
+  const chars = sentence.split("");
 
-const sentence = "What are you looking for?";
-const chars = sentence.split("");
+  sentence.substring(0, index);
 
-setInterval(() => {
-  if (index < chars.length) {
-    searchBar.setAttribute(
-      "placeholder",
-      searchBar.getAttribute("placeholder") + chars[index]
-    );
+  const interval = setInterval(() => {
+    const char = chars[index % chars.length];
+
+    const animatedPlaceholder = sentence.substring(0, index);
+
+    searchBar.placeholder = animatedPlaceholder;
+
     index++;
-  }
-}, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 5000);
+}
+
+window.addEventListener("DOMContentLoaded", animatePlaceholder);
