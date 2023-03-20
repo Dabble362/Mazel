c9f24224b2a98b660bf117947e116f20f9873b52
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-03-20 13:27:41 -0400

    install daisyui on client, update tailwind config js , and remove erroneous files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,60 @@
-import "./App.css";
 import FetchData from "./FetchData";
 
 function App() {
   return (
-    <div className="App">
-      <div class="flex">
-        <section class="xl:flex-1 sm:max-lg:hidden"></section>
-        <section class="flex flex-1 justify-center items-center h-screen">
-          <div class="bg-gradient-to-r from-base-100/75 flex flex-col p-14 md:max-xl:shadow-xl shadow-max-yel-green-500/50">
-            <FetchData />
-            <h1 class="my-8 text-8xl font-bold text-dark-slate-gray lg:backdrop-blur-xs">
-              Mazel
-            </h1>
-            <figcaption class="mb-2 text-dark-slate-gray text-lg font-semibold backdrop-blur-xs">
-              A place to find, talk about, and save the recipes you love.
-            </figcaption>
-            <form
-              method="GET"
-              name=""
-              action="/recipe/searchRecipes"
-              class="flex items-end mb-5"
-            >
-              <div class="form-control w-9/12 max-w-xs">
-                <label class="label">
-                  <span class="label-text text-dark-slate-gray text-lg font-semibold">
-                    What are you looking for?
-                  </span>
-                </label>
-                <input
-                  type="text"
-                  class="bg-white placeholder:text-slate-600 text-slate-600 input input-bordered w-full max-w-xs"
-                  id="searchBar"
-                />
-              </div>
-              <button
-                class="text-key-lime btn rounded-r-lg w-5 bg-forest-green border-none hover:bg-yellow-400"
-                type="submit"
-                id="searchButton"
-              >
-                GO
-              </button>
-            </form>
-            <div>
-              <a
-                class="btn btn-outline btn-success lg:max-xl:bg-[#B5CFC8]"
-                href="/login"
-              >
-                Login
-              </a>
-              <a
-                class="btn btn-outline btn-success lg:max-xl:bg-[#B5CFC8]"
-                href="/signup"
-              >
-                Signup
-              </a>
+    <div class="flex">
+      <section class="xl:flex-1 sm:max-lg:hidden"></section>
+      <section class="flex flex-1 justify-center items-center h-screen">
+        <div class="bg-gradient-to-r from-base-100/75 flex flex-col p-14 md:max-xl:shadow-xl shadow-max-yel-green-500/50">
+          <FetchData />
+          <h1 class="my-8 text-8xl font-bold text-dark-slate-gray lg:backdrop-blur-xs">
+            Mazel
+          </h1>
+          <figcaption class="mb-2 text-dark-slate-gray text-lg font-semibold backdrop-blur-xs">
+            A place to find, talk about, and save the recipes you love.
+          </figcaption>
+          <form
+            method="GET"
+            name=""
+            action="/recipe/searchRecipes"
+            class="flex items-end mb-5"
+          >
+            <div class="form-control w-9/12 max-w-xs">
+              <label class="label">
+                <span class="label-text text-dark-slate-gray text-lg font-semibold">
+                  What are you looking for?
+                </span>
+              </label>
+              <input
+                type="text"
+                class="bg-white placeholder:text-slate-600 text-slate-600 input input-bordered w-full max-w-xs"
+                id="searchBar"
+              />
             </div>
+            <button
+              class="text-key-lime btn rounded-r-lg w-5 bg-forest-green border-none hover:bg-yellow-400"
+              type="submit"
+              id="searchButton"
+            >
+              GO
+            </button>
+          </form>
+          <div>
+            <a
+              class="btn btn-outline btn-success lg:max-xl:bg-[#B5CFC8]"
+              href="/login"
+            >
+              Login
+            </a>
+            <a
+              class="btn btn-outline btn-success lg:max-xl:bg-[#B5CFC8]"
+              href="/signup"
+            >
+              Signup
+            </a>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "280px",
+      sm: "400px",
+      md: "800px",
+      lg: "1024px",
+      xl: "1500px",
+    },
+    maxWidth: {
+      eighty: "80%",
+    },
+    extend: {
+      colors: {
+        "key-lime": "#EDEF8A",
+        "forest-green": "#073D00",
+        "pale-spring-bud": "#E6E7A7",
+        "dark-slate-gray": "#35524A",
+        "caput-mortuum": "#5E3023",
+        "success-green": "#36d399",
+        "max-yel-green": "#DEEf57",
+      },
+      backgroundImage: {
+        "xl-background": "url('../imgs/pexels-elle-hughes-1660030.jpg')",
+        "sm-background": "url('../imgs/pexels-elle-hughes-sm.jpg')",
+        "lg-background": "url('../imgs/pexels-elle-hughes-lg.jpg')",
+      },
+      backdropBlur: {
+        xs: "1px",
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          success: "#073D00",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
