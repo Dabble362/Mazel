4b73eeb6a84c6f82eea57b3882421848ff74d0a8
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-03-28 15:36:43 -0400

    add add-ingredient btn to modal, create modal.js, console log btn click, rename main.js to animatePlaceholder.js

---
+++ public/js/modal.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.querySelector("#add-ingredient-btn");
+
+  addButton.addEventListener("click", () => {
+    console.log("The add ingredient button has been clicked!");
+  });
+});
