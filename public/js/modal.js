442726a12f35dd95ad0ab9f8b5d06c6930c185bf
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-03-28 16:05:12 -0400

    add logic to modal.js to allow for adding of ingredient fields

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addButton = document.querySelector("#add-ingredient-btn");
+  const form = document.querySelector("#recipe-form");
+  let ingredientCount = 2;
 
   addButton.addEventListener("click", () => {
     console.log("The add ingredient button has been clicked!");
+    const input = document.createElement("input");
+    input.type = "text";
+    input.name = `ingredient ${ingredientCount}`;
+    input.placeholder = `ingredient ${ingredientCount}`;
+    input.classList.add(
+      "input",
+      "input-bordered",
+      "w-full",
+      "max-w-xs",
+      "bg-white",
+      "text-dark-slate-gray"
+    );
+    form.insertBefore(input, addButton);
+    ingredientCount++;
   });
 });
