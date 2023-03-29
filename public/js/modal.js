7c3976a6bfbe8ceb5456a65ac44eaf2390bd4956
Daniel Bennett <danielandrew362@gmail.com>
2023-03-29 16:23:50 -0400

    add increase number of steps button to modal.ejs, add logic to that end in modal.js, adjust height of text area.

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addButton = document.querySelector("#add-ingredient-btn");
-  const form = document.querySelector("#recipe-form");
+  const addIngredientButton = document.querySelector("#add-ingredient-btn");
+  const ingredientForm = document.querySelector("#recipe-form");
   let ingredientCount = 2;
 
-  addButton.addEventListener("click", () => {
+  addIngredientButton.addEventListener("click", () => {
     console.log("The add ingredient button has been clicked!");
     const input = document.createElement("input");
     input.type = "text";
@@ -18,7 +18,29 @@ document.addEventListener("DOMContentLoaded", () => {
       "text-dark-slate-gray",
       "my-2"
     );
-    form.insertBefore(input, addButton);
+    ingredientForm.insertBefore(input, addIngredientButton);
     ingredientCount++;
+  });
+
+  const addStepsButton = document.querySelector("#add-steps-btn");
+  const directionsForm = document.querySelector("#directions-form");
+  let stepsCount = 2;
+
+  addStepsButton.addEventListener("click", () => {
+    console.log("The add ingredient button has been clicked!");
+    const textArea = document.createElement("textarea");
+    textArea.type = "text";
+    textArea.name = `step ${stepsCount}`;
+    textArea.placeholder = `step # ${stepsCount}`;
+    textArea.classList.add(
+      "textarea",
+      "textarea-bordered",
+      "h-22",
+      "bg-white",
+      "text-dark-slate-gray",
+      "my-2"
+    );
+    directionsForm.insertBefore(textArea, addStepsButton);
+    stepsCount++;
   });
 });
