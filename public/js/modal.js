0b9d650cb626e80e7ba206864e6b06a530bdaf35
Daniel Bennett <danielandrew362@gmail.com>
2023-05-15 11:09:55 -0400

    MRA-13: Remove search bar from login page (#4)
    * delete search bar
    * adjust margins and sizes on heading and figcaption
    * other minor refactors
    ---------
    Co-authored-by: Jed Duffey <jedduffey@gmail.com>

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,45 +1,51 @@
+const commonClassList = [
+  "bg-white",
+  "text-dark-slate-gray",
+  "my-2",
+]
+
 document.addEventListener("DOMContentLoaded", () => {
   const addIngredientButton = document.querySelector("#add-ingredient-btn");
-  const ingredientForm = document.querySelector("#recipe-form");
   let ingredientCount = 2;
 
   addIngredientButton.addEventListener("click", () => {
     console.log("The add ingredient button has been clicked!");
+
     const input = document.createElement("input");
     input.type = "text";
     input.name = `ingredients[]`;
     input.placeholder = `ingredient ${ingredientCount}`;
+    commonClassList.forEach((className) => input.classList.add(className));
     input.classList.add(
       "input",
       "input-bordered",
       "w-full",
       "max-w-xs",
-      "bg-white",
-      "text-dark-slate-gray",
-      "my-2"
     );
+
+    const ingredientForm = document.querySelector("#recipe-form");
     ingredientForm.insertBefore(input, addIngredientButton);
     ingredientCount++;
   });
 
   const addStepButton = document.querySelector("#add-steps-btn");
-  const directionsForm = document.querySelector("#directions-form");
   let stepsCount = 2;
 
   addStepButton.addEventListener("click", () => {
     console.log("The add step button has been clicked!");
+
     const textArea = document.createElement("textarea");
     textArea.type = "text";
     textArea.name = `directions[]`;
     textArea.placeholder = `step # ${stepsCount}`;
+    commonClassList.forEach((className) => textArea.classList.add(className));
     textArea.classList.add(
       "textarea",
       "textarea-bordered",
       "h-22",
-      "bg-white",
-      "text-dark-slate-gray",
-      "my-2"
     );
+
+    const directionsForm = document.querySelector("#directions-form");
     directionsForm.insertBefore(textArea, addStepButton);
     stepsCount++;
   });
