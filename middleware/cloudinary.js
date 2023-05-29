c038c5655c0876ec87a8412f27f24ad1518d29dd
Daniel Bennett <danielandrew362@gmail.com>
2023-05-29 19:54:12 -0400

    Revert "Bugfix delete recipe button deletes record from database (#14)" (#15)
    This reverts commit c7568a9a4794edb9b0682383a7e3cd74997222d4.

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,8 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-require('dotenv').config();
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
