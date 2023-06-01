c19f36587efc8b741f7eb67d32d9a9417712a595
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-06-01 11:51:42 -0400

    refactor forEach method to not return a value and only to execute codeblock

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -18,7 +18,7 @@ module.exports = {
         .skip(skip)
         .limit(limit);
       console.log(`   The contents of favoriteRecipes`);
-      favoriteRecipes.forEach(recipe => console.log(recipe));
+      favoriteRecipes.forEach(recipe => { console.log(recipe) });
 
       //Sending post data from mongodb and user data to ejs template
       res.render("favorites.ejs", {
