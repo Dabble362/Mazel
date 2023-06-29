35f06dc657bdca44496989be3f0d4c149423f987
Jed Duffey <jedduffey@gmail.com>
2023-06-29 10:59:43 -0400

    Minor; hardcode common arg (text); rename args object to buttonArgs

--- public/js/createRecipe.js
+++ public/js/createRecipe.js
@@ -4,11 +4,10 @@ const commonClassList = [
   "my-2",
 ]
 
-const args = {
+const buttonArgs = {
   ingredients: {
     button: "#add-ingredient-btn",
     input: "input",
-    type: "text",
     name: "ingredients[]",
     placeholderPrefix: "Ingredient",
     classList: ["input", "input-bordered", "w-full", "max-w-xs"],
@@ -18,7 +17,6 @@ const args = {
   steps: {
     button: "#add-steps-btn",
     input: "textarea",
-    type: "text",
     name: "directions[]",
     placeholderPrefix: "Step",
     classList: ["textarea", "textarea-bordered", "h-22"],
@@ -28,18 +26,18 @@ const args = {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  Object.keys(args).forEach((key) => {
-    const button = document.querySelector(args[key].button);
+  Object.keys(buttonArgs).forEach((key) => {
+    const button = document.querySelector(buttonArgs[key].button);
 
     button.addEventListener("click", () => {
-      const element = document.createElement(args[key].input);
-      element.type = args[key].type;
-      element.name = args[key].name;
-      element.placeholder = `${args[key].placeholderPrefix} ${args[key].counter}`;
+      const element = document.createElement(buttonArgs[key].input);
+      element.type = "text";
+      element.name = buttonArgs[key].name;
+      element.placeholder = `${buttonArgs[key].placeholderPrefix} ${buttonArgs[key].counter}`;
       commonClassList.forEach((className) => element.classList.add(className));
-      element.classList.add(...args[key].classList);
-      document.querySelector(args[key].parent).insertBefore(element, button);
-      args[key].counter += 1;
+      element.classList.add(...buttonArgs[key].classList);
+      document.querySelector(buttonArgs[key].parent).insertBefore(element, button);
+      buttonArgs[key].counter += 1;
     });
   });
 });
